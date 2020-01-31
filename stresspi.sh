ed4632faeee7d50f3edd9215f5b9b8fbfed5a3cb
Vincent Arnaud <vincent.arnaud@sculpteo.com>
2020-01-31 15:50:42 +0000

    typo

--- stresspi.sh
+++ stresspi.sh
@@ -6,7 +6,8 @@ logfile=stresspi_$(date +%F-%H-%M-%S).log
 stress -c $cpus -t $end &
 while [ $SECONDS -lt $end ]; do
     temp=$(cat /sys/class/thermal/thermal_zone0/temp)
-    echo $(($temp/1000)) | tee -a $logfile
+    freq=$(vcgencmd measure_clock arm | cut -d"=" -f2)
+    echo $(echo "scale=2;$temp/1000" | bc) $(echo "scale=2;$freq/1000000" | bc) | tee -a $logfile
     sleep 1
 done &
 wait
