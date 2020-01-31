e7e0ad79c75334bd87c2227cf254988441795622
Vincent Arnaud <vincent.arnaud@sculpteo.com>
2020-01-31 16:11:41 +0000

    Change to for loop, add cleanup in case of ctrl-c

--- stresspi.sh
+++ stresspi.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
-end=60
+end=10
 cpus=$(nproc --all)
 logfile=stresspi_$(date +%F-%H-%M-%S).log
-stress -c $cpus -t $end &
-while [ $SECONDS -lt $end ]; do
+cleanup () {
+    killall stress
+    exit 2
+}
+trap cleanup SIGINT
+
+# start stressing the pi
+stress -c $cpus &
+
+# monitor and log temperature and cpu clock speed
+# update every second
+for ((i = 0; i <= $end; i++)); do
     temp=$(cat /sys/class/thermal/thermal_zone0/temp)
     freq=$(vcgencmd measure_clock arm | cut -d"=" -f2)
     echo $(echo "scale=2;$temp/1000" | bc) $(echo "scale=2;$freq/1000000" | bc) | tee -a $logfile
     sleep 1
-done &
-wait
+done
+killall stress
