2cff827f48eda06381601c0eaae8a34cd3d6f028
Vincent Arnaud <vincent.arnaud@sculpteo.com>
2020-01-31 19:02:03 +0000

    Remove frequency logging

--- stresspi.sh
+++ stresspi.sh
@@ -13,10 +13,12 @@ stress -c $cpus &
 
 # monitor and log temperature and cpu clock speed
 # update every second
-for ((i = 0; i <= $end; i++)); do
+for ((i = 0; i < $end; i++)); do
     temp=$(cat /sys/class/thermal/thermal_zone0/temp)
-    freq=$(vcgencmd measure_clock arm | cut -d"=" -f2)
-    echo $(echo "scale=2;$temp/1000" | bc) $(echo "scale=2;$freq/1000000" | bc) | tee -a $logfile
+    temp_val=$(echo "scale=2;$temp/1000" | bc)
+    # freq=$(vcgencmd measure_clock arm | cut -d"=" -f2)
+    # freq_val=$(echo "scale=2;$freq/1000000" | bc)
+    echo $temp_val | tee -a $logfile
     sleep 1
 done
 killall stress
